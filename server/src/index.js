a7fcfbaa12d241d2ad6ef461bf33f72f24cf57b4
neovicon <240396sarun@cosmoscollege.edu.np>
2026-05-26 12:59:23 +0545

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -79,8 +79,10 @@ app.all('*', (req, res, next) => {
 // Global error handler
 app.use(errorHandler);
 
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
