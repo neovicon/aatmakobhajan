ab0700e7a53900e6bbe33a4566497300e3e01711
neovicon <240396sarun@cosmoscollege.edu.np>
2026-05-26 13:30:23 +0545

    Added site to cors

--- server/src/index.js
+++ server/src/index.js
@@ -32,7 +32,7 @@ const app = express();
 // Security Middlewares
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173' || 'https://aatmakobhajan.onrender.com',
   credentials: true,
 }));
 
